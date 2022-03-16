3281c8e4d3d657319e75932bfa6215892921d2eb
Thomas Prévost <thomasprevost85@gmail.com>
2022-03-16 19:55:48 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,149 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+PS Generator
+@author: PRV
+@version: 1.0.0
+ */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import { withStyles } from '@material-ui/core/styles';
+import {EmailOutlined, FileCopy, FileCopyOutlined, FileCopyRounded} from "@material-ui/icons";
+import {IconButton, InputAdornment, Snackbar} from "@mui/material";
+
+
+const styles = theme => ({
+
+  '@global': {
+    body: {
+      backgroundColor: theme.palette.common.white,
+    },
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+    display: 'fill',
+    alignItems: 'center',
+    flexDirection: 'row',
+
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+    width: '100pt',
+  },
+  copy: {
+    margin: theme.spacing(3, 6, 2),
+    //width: '100pt',
+  }
+});
+
+let open=false;
+
+class PsGenerator extends React.Component {
+
+
+  state = {
+    userName: '',
+    password: '',
+  }
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Container component="main" maxWidth="xl">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <EmailOutlined />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Générateur de PS
+          </Typography>
+
+          <form className={classes.form} noValidate>
+            <TextField
+              width="20%"
+              id='nbrPyramids'
+              type="number"
+              defaultValue={1}
+              label="Pyramides"
+              variant="outlined"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="usrinput"
+              label="Votre texte"
+              name="userinput"
+              autoFocus
+              multiline
+              rows={8}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              fullWidth
+              name="txtresult"
+              label="Vos 💫merveilleux💫 PS"
+              type="text"
+              id="txtresult"
+              multiline
+              InputProps={{
+                readOnly: true,
+
+/*
+              endAdornment:(
+                <InputAdornment position="end">
+                  <FileCopy
+                    aria-label="copy to clipboard"
+                    onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}}
+                    edge="end"
+                  />
+                </InputAdornment>),
+*/
+              }}
+            />
+            <Grid container>
+
+            </Grid>
+          </form>
+          <div>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            id="btn"
+            className={classes.submit}
+          >
+            Ça part !
+          </Button><Button className={classes.copy}>
+            <FileCopyRounded
+            onClick={() => {navigator.clipboard.writeText(document.getElementById('txtresult').value); alert("Copié !");}}/>
+          </Button>
+          </div>
+
+        </div>
+      </Container>
+    );
+  }
 }
 
-export default App;
+export default withStyles(styles)(PsGenerator);
+//export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Button from '@mui/material/Button';
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,6 +12,86 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+document.getElementById("btn").addEventListener("click", gen);
+
+function gen(){
+  let s = document.getElementById("usrinput").value.toString();
+  const n = document.getElementById('nbrPyramids').value;
+  let L = s.split('\n');
+  var res = "";
+  if (n==1){
+    res = pyramid(L);
+  } else{
+    var M = chunkify(L, n, true);
+    for (const el of M){
+      res += pyramid(el);
+    }
+  }
+  console.log(res);
+  document.getElementById('txtresult').value = res.toString();
+  document.getElementById('txtresult').focus();
+}
+
+function pyramid(L){
+  var s = "";
+  var j = L.length;
+  let len = L.length;
+  var P = "P";
+  console.log(P, len);
+  for (let i = 1; i <= len; i++){
+    if (i <= Math.floor(len / 2)){
+      P = "P".repeat(i);
+    } else{
+      P = "P".repeat(j);
+    }
+    console.log("i = "+i,"j = "+ j);
+    let tmp = P+'S: '+L[i-1];
+    s += tmp + '\n';
+    j--;
+  }
+  return s;
+}
+
+function chunkify(L, n, balanced) {
+
+  if (n < 2)
+    return [L];
+
+  var len = L.length,
+    out = [],
+    i = 0,
+    size;
+
+  if (len % n === 0) {
+    size = Math.floor(len / n);
+    while (i < len) {
+      out.push(L.slice(i, i += size));
+    }
+  }
+
+  else if (balanced) {
+    while (i < len) {
+      size = Math.ceil((len - i) / n--);
+      out.push(L.slice(i, i += size));
+    }
+  }
+
+  else {
+
+    n--;
+    size = Math.floor(len / n);
+    if (len % size === 0)
+      size--;
+    while (i < size * n) {
+      out.push(L.slice(i, i += size));
+    }
+    out.push(L.slice(size * n));
+
+  }
+
+  return out;
+}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
